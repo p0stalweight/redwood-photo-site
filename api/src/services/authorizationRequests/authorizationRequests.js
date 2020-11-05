ede55066b60d99f997fa5ec3cf4b9ec355aad623
John Postlewaite <p0stalweight@gmail.com>
2020-11-05 15:07:52 -0800

    fetching upload url api call in the service is working

--- api/src/services/authorizationRequests/authorizationRequests.js
+++ api/src/services/authorizationRequests/authorizationRequests.js
@@ -12,28 +12,25 @@ export const getAuthorizationRequest= async() => {
 
   const response = await fetch(url, {headers: headers})
   const authorizationJson = await response.json()
-  console.log(authorizationJson)
+  await console.log("/////  AUTH JSON  ////")
+  await console.log(authorizationJson)
 
   /* Request the upload url */
   let bucketId = 'ce0b002c732dcc2070490713'
   let uploadRequestUrl = await authorizationJson.apiUrl + '/b2api/v2/b2_get_upload_url'
   const uploadRequestHeaders = await {Authorization: `${authorizationJson.authorizationToken}`}
-  const uploadRequestBody = JSON.stringify({ bucketId: `${bucketID}` })
+  const uploadRequestBody = JSON.stringify({ bucketId: `${bucketId}` })
 
   const uploadResponse = await fetch(uploadRequestUrl, {method: 'Post', headers: uploadRequestHeaders, body: uploadRequestBody})
-  const uploadJson = await uploadResponse.json
-
-  /*fetch(proxyURL + targetURL, {
-      method: 'POST',
-      headers: new Headers({
-        Authorization: `${authToken}`,
-      }),
-      body: JSON.stringify({ bucketId: `${bucketID}` })*/
+  const uploadJson = await uploadResponse.json()
+  console.log("/////  UPLOAD JSON  ////")
+  console.log(uploadJson)
 
   return {
     authorizationToken: authorizationJson.authorizationToken,
     backblazeApiUrl: authorizationJson.apiUrl,
     backblazeDownloadUrl: authorizationJson.downloadUrl,
     backblazeUploadUrl: uploadJson.uploadUrl,
-    backblazeUploadAuthToken: uploadJson.authorizationToken,
+    backblazeUploadAuthToken: uploadJson.authorizationToken
+  }
 }
