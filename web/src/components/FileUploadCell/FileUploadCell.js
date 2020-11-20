8dd4ab7cfb3af19f8300509edcd5c99045dfb918
John Postlewaite <p0stalweight@gmail.com>
2020-11-20 11:54:28 -0800

    turned upload back on

--- web/src/components/FileUploadCell/FileUploadCell.js
+++ web/src/components/FileUploadCell/FileUploadCell.js
@@ -123,8 +123,6 @@ export const Success = ({ authorizationRequest }) => {
 
   const submitGallery = async(formData) => {
     // Send photos to Backblaze
-    console.log("Upload disabled")
-    return
     if (images.length < 1) {
       console.log("No photos to upload, cancelled")
       return
